a8543cf92b0c79673774334b80af9e6399fcb123
Bridget Klingler <bridget.klingler@gmail.com>
2019-06-27 00:21:07 -0400

    first commit with minimal structure

---
+++ GardenBlog/BlogContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using GardenBlog.Models;
+
+namespace GardenBlog
+{
+    public class BlogContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = "Server=(localdb)\\mssqllocaldb;Database=SiteDb;Trusted_Connection=True;";
+            optionsBuilder.UseSqlServer(connectionString)
+                        .UseLazyLoadingProxies();
+            base.OnConfiguring(optionsBuilder); }
+    }
+}
---
+++ GardenBlog/Models/Post.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GardenBlog.Models
+{
+    public class Post
+    {
+    }
+}
