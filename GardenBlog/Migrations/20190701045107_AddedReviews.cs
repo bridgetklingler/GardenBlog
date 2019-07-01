45df46ca749da8ee59e1158905e6093ec4418a4b
Blair <bgriffit@oberlin.edu>
2019-07-01 00:55:54 -0400

    new migrations, WIP

---
+++ GardenBlog/Migrations/20190701045107_AddedReviews.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GardenBlog.Migrations
+{
+    public partial class AddedReviews : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Posts",
+                keyColumn: "PostId",
+                keyValue: 1,
+                column: "TimeStamp",
+                value: new DateTime(2019, 7, 1, 0, 51, 7, 148, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Posts",
+                keyColumn: "PostId",
+                keyValue: 2,
+                column: "TimeStamp",
+                value: new DateTime(2019, 7, 1, 0, 51, 7, 150, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Posts",
+                keyColumn: "PostId",
+                keyValue: 3,
+                column: "TimeStamp",
+                value: new DateTime(2019, 7, 1, 0, 51, 7, 150, DateTimeKind.Local));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Posts",
+                keyColumn: "PostId",
+                keyValue: 1,
+                column: "TimeStamp",
+                value: new DateTime(2019, 6, 30, 23, 41, 3, 944, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Posts",
+                keyColumn: "PostId",
+                keyValue: 2,
+                column: "TimeStamp",
+                value: new DateTime(2019, 6, 30, 23, 41, 3, 946, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Posts",
+                keyColumn: "PostId",
+                keyValue: 3,
+                column: "TimeStamp",
+                value: new DateTime(2019, 6, 30, 23, 41, 3, 946, DateTimeKind.Local));
+        }
+    }
+}
