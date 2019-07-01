1155eeb42517d408d69e4551cec06878fe15d5f8
Bridget Klingler <bridget.klingler@gmail.com>
2019-07-01 02:56:40 -0400

    added easy references for css to make all buttons the same, added title spacing, got the home page looking nice and hovers working

--- GardenBlog/BlogContext.cs
+++ GardenBlog/BlogContext.cs
@@ -33,43 +33,118 @@ namespace GardenBlog
                     PostId = 1,
                     CategoryId = 1,
                     Author = "Blair",
-                    Title = "Horror",
-                    Body = "This was so scary, it's scary",
+                    Title = "Spider Plant",
+                    Body = "I've found that the spider plant is the most effective at cleaning the air in my home. As someone with asthma, I can attest that this plant has done wonders for my breathing!",
                     TimeStamp = DateTime.Now,
                 },
-                
                 new Post
                 {
                     PostId = 2,
                     CategoryId = 1,
                     Author = "Bridget",
-                    Title = "Comedy",
-                    Body = "Soooo funny",
+                    Title = "English Ivy",
+                    Body = "I know this plant is easy to care for inside, but does anyone else have an issue with the ivy climbing on every other plant within a 5 mile radius? Asking for a friend...",
+                    TimeStamp = DateTime.Now,
+                },
+                new Post
+                {
+                    PostId = 3,
+                    CategoryId = 1,
+                    Author = "Kyle",
+                    Title = "Burning Bush",
+                    Body = "I just planted a burning bush off my back porch and the leaves all fell off within the week. Is it dead or just didn't take the transplant well?",
+                    TimeStamp = DateTime.Now,
+                },
+                new Post
+                {
+                    PostId = 4,
+                    CategoryId = 2,
+                    Author = "Elise",
+                    Title = "Asiatic Lilies",
+                    Body = "Last year I planted 4 asiatic lily plants and after my dog dug them up on multiple occassions, I just assumed they were dead. But this year, I have 9 plants growing! I'm so excited!",
                     TimeStamp = DateTime.Now,
                 },
 
                 new Post
                 {
-                    PostId = 3,
+                    PostId = 5,
+                    CategoryId = 2,
+                    Author = "Peter",
+                    Title = "Lily Pads",
+                    Body = "I have a little bird bath fountain that I just put lily pads in. Now I have a couple peepers and the birds seem to not be using it anymore. Do you think the frogs scared them away? Or are birds scared of lily pads and I just didn't know about it?",
+                    TimeStamp = DateTime.Now,
+                },
+
+                new Post
+                {
+                    PostId = 6,
+                    CategoryId = 1,
+                    Author = "Miranda",
+                    Title = "Sunflowers",
+                    Body = "I just came across this entire acre field that plants sunflowers every year outside of Avon. It's so gorgeous, you have to check it out! It's so bright and yellow, and such a fun walk thorugh the field, it's a must do in the 2 weeks they're in full bloom!",
+                    TimeStamp = DateTime.Now,
+                },
+
+                new Post
+                {
+                    PostId = 7,
                     CategoryId = 2,
                     Author = "Travis",
-                    Title = "Horror",
-                    Body = "Kinda Creepy",
+                    Title = "Mulch",
+                    Body = "Black mulch beware!! I've found that the black mulch actually has overheated my softer plants. Just make sure to keep a good eye out on your plants after laying mulch. The black mulch tends to fade rather quickly, so the first 2 weeks are key.",
                     TimeStamp = DateTime.Now,
                 });
+
 
             modelBuilder.Entity<Tag>().HasData(
                 new Tag
                 {
                     TagId = 1,
-                    TagName = "funny",
+                    TagName = "mulch",
                 },
 
                 new Tag
                 {
                     TagId = 2,
-                    TagName = "scary"
-                });
+                    TagName = "ivy"
+                },
+
+                 new Tag
+                 {
+                     TagId = 3,
+                     TagName = "orange",
+                 }, 
+                 
+                 new Tag
+                 {
+                     TagId = 4,
+                     TagName = "colorful",
+                 },
+                 
+                 new Tag
+                 {
+                     TagId = 5,
+                     TagName = "high-maintenance",
+                 }, 
+                 
+                 new Tag
+                 {
+                     TagId = 6,
+                     TagName = "easy-watering",
+                 }, 
+                 
+                 new Tag
+                 {
+                     TagId = 7,
+                     TagName = "direct-sun",
+                 },
+
+                 new Tag
+                 {
+                     TagId = 8,
+                     TagName = "low-maintenance",
+                 });
+
 
             modelBuilder.Entity<PostTag>().HasData(
                 new PostTag
@@ -87,6 +162,7 @@ namespace GardenBlog
                     PostId = 3,
                     TagId = 2
                 });
+
 
             modelBuilder.Entity<Category>().HasData(
                 new Category
